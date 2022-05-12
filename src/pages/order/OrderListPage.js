1494c69a8ca98b53cebe127a7dd5c5eb1694f943
Muhammad Rafif Elfazri <rafif.elfazri@gmail.com>
2022-05-12 16:04:26 +0700

    add order detail page

--- src/pages/order/OrderListPage.js
+++ src/pages/order/OrderListPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import styles from './OrderList.module.css'
+import { Container } from 'react-bootstrap'
+import styles from './Order.module.css'
 
 export const OrderListPage = () => {
      return(
@@ -30,6 +30,7 @@ export const OrderListPage = () => {
             <Container className={`${styles.boxColor} py-3 d-flex flex-column justify-content-md-start`}>
               <h3>Order 123</h3>
               <p className={'fs-6'}>28 April 2000</p>
+              <p className={'fs-6'}>Item List: Delivering</p>
               <p className={'fs-6'}>Item List:</p>
               <ul className='fs-6'>
                 <li>
--- src/routes/ApplicationRoutes.js
+++ src/routes/ApplicationRoutes.js
@@ -5,6 +5,7 @@ import {
 import { ExamplePage } from '../pages/example/ExamplePage'
 import { EWalletPage } from '../pages/e-wallet/EWalletPage'
 import { OrderListPage } from '../pages/order/OrderListPage'
+import { OrderDetailPage } from '../pages/order/OrderDetailPage'
 
 export const ApplicationRoutes = () => {
      return(
@@ -12,6 +13,7 @@ export const ApplicationRoutes = () => {
                <Route path='/' element={<ExamplePage/>}/>
                <Route path='/e-wallet' element={<EWalletPage/>}/>
                <Route path='/order' element={<OrderListPage/>}/>
+               <Route path='/order/detail' element={<OrderDetailPage/>}/>
           </Routes>
      )
 }
