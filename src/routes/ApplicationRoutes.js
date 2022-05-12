bc3ca72dee40bb9d54d1b160c1deec022ef7f2ab
Johanes Simarmata <jmpsimarmata@gmail.com>
2022-05-12 22:20:18 +0700

    add application product detail route to enable review component

--- src/routes/ApplicationRoutes.js
+++ src/routes/ApplicationRoutes.js
@@ -4,11 +4,14 @@ import {
  } from 'react-router-dom'
 import { ExamplePage } from '../pages/example/ExamplePage'
 import { EWalletPage } from '../pages/e-wallet/EWalletPage'
+import { ProductDetails } from '../pages/product-details/ProductDetails'
+
 export const ApplicationRoutes = () => {
      return(
           <Routes>
                <Route path='/' element={<ExamplePage/>}/>
                <Route path='/e-wallet' element={<EWalletPage/>}/>
+               <Route path='/product-details/:idproduct' element={<ProductDetails/>}/>
           </Routes>
      )
 }
